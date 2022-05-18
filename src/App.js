9718c55795215ede8a81bc9fce420d1b401f494b
Mateusz Zambrzycki <you@example.com>
2022-05-18 22:54:26 +0200

    dsa2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/login_component'
-import After_Login from './components/after_login'
+
+
 function App() {
   return (
     <div className="App">
       
       <Login/>
-      <After_Login/>
 
     </div>
   );
--- src/components/login_component.js
+++ src/components/login_component.js
@@ -4,20 +4,81 @@ import axios from 'axios'
 
 
 
+
+
 const Login = () => {
 
+  const [page,setPage] = useState(7)
 
+console.log(page)
 
+  useEffect(()=>{
+    var config = {
+      method: 'get',
+      url: 'https://api.themoviedb.org/3/movie/upcoming?api_key=6bba403051b76af0cbe8fa3816db1c9b&page='+page,
+      headers: { }
+    };
+    
+    axios(config)
+    .then(function (response) {
+      const relace_place = document.querySelector('.relase-place');
+    
+    
+    
+      for(let i = 0; i < response.data.results.length; i++){
+            var title =  response.data.results[i].title;
+            var overview = response.data.results[i].overview;
+          var image = response.data.results[i].poster_path;
+          
+          if(image == null){
+            var image_url = "https://via.placeholder.com/300x450";
+          }else{
+            var image_url = 'https://image.tmdb.org/t/p/original' + image;
+          }
+         
+          if(overview.length <= 5){
+            var overview  = "Overview will be available soon !";
+          }
 
+  
+
+          (relace_place.innerHTML +=  '<div class="box">' +'<div class="poster-holder">'  + title   + '<img class="posterimg" src=' + image_url +'>'    + '</div>'    + '<div class="overview">'  + overview + '</div>'  + '<br>'  +  '</div>'        )
+       console.log(response.data.results[i].title)
+      }
+      
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    
+  },[page])
+    
 
 
 
         return(
           
         <header className="App-header" >
-          <div className="relase-place" >
-             
+<div className="relase-holder">
+      <div className="advanced-search">
+<div className="advanced-search-header">
+  Category
+          <select >
+            <option>Horror</option>
+            <option>Horror2</option>
+          </select>
+<button></button>
+</div>
+        
+      </div>
+          <div className="relase-place" >    
           </div> 
+       
+
+          </div>
+          <button className="scroll_down" onClick={(_)=>{
+            setPage(page+1)
+          }}></button>
         </header>
       )    
 
